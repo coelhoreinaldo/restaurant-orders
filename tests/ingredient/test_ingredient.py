e4bc2586ea9db5549796516ac406bd44448f6da3
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-06 16:08:47 -0300

    🧪 test: test ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,14 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    banana = Ingredient("banana")
+
+    assert banana.name == "banana"
+
+    assert banana.restrictions == set()
+
+    assert banana.__hash__() == hash("banana")
+
+    assert banana.__eq__(Ingredient("banana"))
+
+    assert banana.__repr__() == "Ingredient('banana')"
