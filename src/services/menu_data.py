0c643eb80fcbaa87073fd7399124d960f4dce6d0
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-06 19:20:49 -0300

    ✨ feat: read data

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,14 @@
-# Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.source_path = source_path
+
+    def load_data(self):
+        with open(self.source_path, "r") as file:
+            lines = file.readlines()
+            lines = [line.strip().split(",") for line in lines]
+            lines.pop(0)
+            return lines
+
+
+teste = MenuData("data/menu_base_data.csv")
+print(teste.load_data())
