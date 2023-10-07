26aa10360fbd42215a88cc42d5b9a8bae8e50a59
coelhoreinaldo <scoelhor7@gmail.com>
2023-10-07 07:41:28 -0300

    ✨ feat: req 3, menu_data

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,14 +1,34 @@
+import csv
+from models.dish import Dish
+from models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.source_path = source_path
+        self.dishes = self.load_data()
 
     def load_data(self):
         with open(self.source_path, "r") as file:
-            lines = file.readlines()
-            lines = [line.strip().split(",") for line in lines]
-            lines.pop(0)
-            return lines
+            lines = csv.reader(file, delimiter=",", quotechar='"')
+            header, *data = lines
+
+            dishes_collection = set()
+
+            for dish_name, price, ingredient, amount in data:
+                curr_dish = Dish(dish_name, float(price))
+                dishes_collection.add(curr_dish)
+                for dish in list(dishes_collection):
+                    if dish.name == dish_name:
+                        dish.add_ingredient_dependency(
+                            Ingredient(ingredient),
+                            int(amount),
+                        )
+
+            return dishes_collection
 
 
-teste = MenuData("data/menu_base_data.csv")
-print(teste.load_data())
+# menu_data = MenuData("data/menu_base_data.csv")
+# menu_data_dishes = list(menu_data.dishes)
+# print(menu_data_dishes[0].get_ingredients())
+# print(menu_data_dishes[1].get_ingredients())
